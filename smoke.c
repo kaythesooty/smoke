0da42e13ff6bb022cd2d3b12413c155b4e6f45e0
Riley Wessel <lessewyendor@gmail.com>
2024-10-20 12:12:34 +1300

    Add list games functions

--- smoke.c
+++ smoke.c
@@ -2,32 +2,74 @@
 #include <string.h>
 
 void init();
+void list(FILE *cfg);
+void find(FILE *cfg, char game[]);
 
-int main(int argc, char **argv) {
-    // init();
-
+int main(int argc, char *argv[])
+{
     FILE *cfg;
     cfg = fopen("smoke.cfg", "r");
 
-    if (argc > 1) {
-        char game[24];
-        fscanf(cfg, "%24s", &game);
-        if (strcmp(game, argv[1]) == 0) {
-            printf("Found %s vs %s\n", argv[1], game);
-        } else {
-            printf("Couldn't find %s vs %s\n", argv[1], game);
-            // return 1;
-        }
+    // if there were no cfg it would be necessary to create it
+    if(cfg == NULL) {
+        init(cfg);
     }
 
-    // printf("%c\n", argv[1][0]);
+    if (argc > 1) {
+        find(cfg, argv[1]);
+    } else {
+        list(cfg);
+    }
 
     return 0;
 }
 
-void init() {
-    FILE *cfg;
-
+void init(FILE *cfg)
+{
     cfg = fopen("smoke.cfg", "w");
     fclose(cfg);
+
+    printf("No cfg file found, new one created.\n");
+}
+
+void list(FILE *cfg)
+{
+    char buffer[128];
+
+    printf("----------\n");
+
+    while (fgets(buffer, 128, cfg)) {
+
+        if (buffer[0] != '#' && buffer[0] != '\n') {
+            printf("%s", buffer);
+            fgets(buffer, 128, cfg);
+            printf("Name: %s", buffer);
+            fgets(buffer, 128, cfg);
+            printf("Path: %s\n", buffer);
+
+        }
+
+    }
+
+}
+
+void find(FILE *cfg, char game[])
+{
+    char buffer[128];
+
+    printf("----------\n");
+
+    while (fgets(buffer, 128, cfg)) {
+
+        if (strspn(buffer, game) >= strlen(game) && buffer[0] != '#') {
+            printf("%s", buffer);
+            fgets(buffer, 128, cfg);
+            printf("Name: %s", buffer);
+            fgets(buffer, 128, cfg);
+            printf("Path: %s\n", buffer);
+
+        }
+
+    }
+
 }
