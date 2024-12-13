9b38aceef8d7b0be3832e1bf57a298cf54a95e0d
Riley Wessel <lessewyendor@gmail.com>
2024-12-14 11:22:33 +1300

    shorten indentation

--- smoke.c
+++ smoke.c
@@ -13,58 +13,58 @@ void play(FILE *cfg, char game[]);
 
 int main(int argc, char *argv[])
 {
-    char *home = getenv("HOME");
-    char *pathend = "/.config/smoke.cfg";
+  char *home = getenv("HOME");
+  char *pathend = "/.config/smoke.cfg";
 
-    char path[100];
-    strcpy(path, home);
-    strcat(path, pathend);
+  char path[100];
+  strcpy(path, home);
+  strcat(path, pathend);
 
-    FILE *cfg;
-    cfg = fopen(path, "r");
+  FILE *cfg;
+  cfg = fopen(path, "r");
 
-    // if there were no cfg it would be necessary to create it
-    if(cfg == NULL) {
-        init(cfg, path);
-        return 1;
-    }
+  // if there were no cfg it would be necessary to create it
+  if(cfg == NULL) {
+    init(cfg, path);
+    return 1;
+  }
 
-    if (argc >= 2 && argv[1][0] != '-') play(cfg, argv[1]);
-    if (argc >= 2 && argv[1][0] == '-')
+  if (argc >= 2 && argv[1][0] != '-') play(cfg, argv[1]);
+  if (argc >= 2 && argv[1][0] == '-')
+  {
+    switch (argv[1][1])
     {
-        switch (argv[1][1])
-        {
-            case 'h':
-                help();
-                return 0;
-            case 'l':
-                list(cfg, argv[2]);
-                return 0;
-            case 'a':
-                addg(cfg, argc, argv, path);
-                return 0;
-            case 'r':
-                printf("todo: remove game\n");
-                return 0;
-            default:
-                printf("\x1b[31mError: Invalid parameter\x1b[0m\n");
-                help();
-                return 0;
-        }
+      case 'h':
+        help();
+        return 0;
+      case 'l':
+        list(cfg, argv[2]);
+        return 0;
+      case 'a':
+        addg(cfg, argc, argv, path);
+        return 0;
+      case 'r':
+        printf("todo: remove game\n");
+        return 0;
+      default:
+        printf("\x1b[31mError: Invalid parameter\x1b[0m\n");
+        help();
+        return 0;
     }
-    if (argc == 1) help();
+  }
+  if (argc == 1) help();
 
-    fclose(cfg);
-    return 0;
+  fclose(cfg);
+  return 0;
 }
 
 bool filter(char *str1, char *str2)
 {
-    if (!str2) return true;
+  if (!str2) return true;
 
-    if (strspn(str1, str2) >= strlen(str2)) return true;
+  if (strspn(str1, str2) >= strlen(str2)) return true;
 
-    return false;
+  return false;
 }
 
 void addg(FILE *cfg, int argc, char *argv[], char cfgpath[])
@@ -113,34 +113,34 @@ void addg(FILE *cfg, int argc, char *argv[], char cfgpath[])
 
 void init(FILE *cfg, char path[])
 {
-    cfg = fopen(path, "w");
-    fclose(cfg);
+  cfg = fopen(path, "w");
+  fclose(cfg);
 
-    printf("No cfg file found, new one created.\n");
+  printf("No cfg file found, new one created.\n");
 }
 
 void list(FILE *cfg, char game[])
 {
-    char buffer[128];
-    bool found = false;
+  char buffer[128];
+  bool found = false;
 
-    printf("----------\n");
+  printf("----------\n");
 
-    while (fgets(buffer, 128, cfg)) {
+  while (fgets(buffer, 128, cfg)) {
 
-        if (filter(buffer, game) && buffer[0] != '#' && buffer[0] != '\n') {
-            printf("%s", buffer);
-            fgets(buffer, 128, cfg);
-            printf("Name: %s", buffer);
-            fgets(buffer, 128, cfg);
-            printf("Path: %s\n", buffer);
-            found = true;
-
-        }
+    if (filter(buffer, game) && buffer[0] != '#' && buffer[0] != '\n') {
+      printf("%s", buffer);
+      fgets(buffer, 128, cfg);
+      printf("Name: %s", buffer);
+      fgets(buffer, 128, cfg);
+      printf("Path: %s\n", buffer);
+      found = true;
 
     }
 
-    if (!found) printf("No games match\n");
+  }
+
+  if (!found) printf("No games match\n");
 
 }
 
@@ -153,21 +153,21 @@ void help() {
 
 void play(FILE *cfg, char game[])
 {
-    char buffer[128];
+  char buffer[128];
 
-    while(fgets(buffer, 128, cfg))
+  while(fgets(buffer, 128, cfg))
+  {
+    buffer[strcspn(buffer, "\n")] = 0;
+    if (strcmp(buffer, game) == 0 && buffer[0] != '#' && buffer[0] != '\n')
     {
-        buffer[strcspn(buffer, "\n")] = 0;
-        if (strcmp(buffer, game) == 0 && buffer[0] != '#' && buffer[0] != '\n')
-        {
-            fgets(buffer, 128, cfg);
-            printf("Running %s", buffer);
-            fgets(buffer, 128, cfg);
-            printf("from %s", buffer);
+      fgets(buffer, 128, cfg);
+      printf("Running %s", buffer);
+      fgets(buffer, 128, cfg);
+      printf("from %s", buffer);
 
-            return;
-        }
+      return;
     }
+  }
 
-    printf("%s not found\n", game);
+  printf("%s not found\n", game);
 }
